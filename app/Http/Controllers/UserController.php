a440d8ea491152ea0009aaca91ec8e41b690c439
Luis Zelaya <codewithluis11@gmail.com>
2023-03-10 23:22:18 -0600

    Seccion Consultas Avanzadas y Rendimiento: where, whereHas y mas

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,61 @@ class UserController extends Controller
 {
     public function index(): View
     {
+        //EJEMPLO has
+        // $users = User::query()
+        //     ->has('projects')
+        //     ->with('projects')
+        //     ->get();
+
+        //EJEMPLO has CON FILTRO
+        // $users = User::has('projects', '>', 1)
+        //     ->get();
+
+        //EJEMPLO whereHas
+        // $users = User::query()
+        //     ->whereHas('projects', function (Builder $query) {
+        //         $query->where('is_active', 1);
+        //     })
+        //     ->get();
+
+        //EJEMPLO whereHas CON with COMBINADO
+        // $users = User::query()
+        //     ->whereHas('projects', function (Builder $query) {
+        //         $query->where('is_active', 1);
+        //     })
+        //     ->with('projects', function ($query) {
+        //         $query->where('is_active', 1);
+        //     })
+        //     ->get();
+
+        //EJEMPLO whereHas CON FILTRO EXTRA
+        // $users = User::query()
+        //     ->whereHas('projects', function (Builder $query) {
+        //         $query->where('name', 'like', '%qui%');
+        //     }, '>=', 2)
+        //     ->get();
+
+        ////EJEMPLO withWhereHas
+        // $users = User::query()
+        //     ->withWhereHas('projects', function ($query) {
+        //         $query->where('is_active', 1);
+        //     })
+        //     ->get();
+
+        // //EJEMPLO whereRelation
+        // $users = User::query()
+        //     ->whereRelation('projects', 'is_active', 1)
+        //     ->get();
+        //
+
+        //EJEMPLO whereRelation con FILTRO
+        // $users = User::query()
+        //     ->whereRelation('projects', 'id', '>', 5)
+        //     ->get();
+
+        //EJEMPLO whereDoesntHave
         $users = User::query()
-            ->with('projects')
+            ->whereDoesntHave('projects')
             ->get();
 
         return view('users.index', compact('users'));
