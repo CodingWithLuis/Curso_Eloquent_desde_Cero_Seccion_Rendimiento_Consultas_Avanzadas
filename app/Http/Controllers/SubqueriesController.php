c588cc11f9d24cd9d4a3768b1af274911f77e832
Luis Zelaya <codewithluis11@gmail.com>
2023-03-18 19:32:34 -0600

    Seccion Rendimiento Y Consultas Avanzadas: SubQueries

--- app/Http/Controllers/SubqueriesController.php
+++ app/Http/Controllers/SubqueriesController.php
@@ -2,12 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Employee;
+use App\Models\Login;
+use App\Models\Product;
+use App\Models\Project;
+use App\Models\Sale;
+use App\Models\User;
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\View\View;
 
 class SubqueriesController extends Controller
 {
     public function index(): View
     {
-        return view('subqueries.index');
+
+        //EJEMPLO 1
+        $projects = Project::query()
+            ->with('user')
+            ->orderBy(User::select('name')->whereColumn('users.id', 'projects.user_id'), 'DESC')
+            ->get();
+
+        //EJEMPLO 2
+        $products = Product::query()
+            ->select('name', 'price')
+            ->where(
+                'price',
+                '>',
+                (Product::avg('price'))
+            )
+            ->get();
+
+        //EJEMPLO 3
+        $sales = Sale::query()
+            ->select('employee_id')
+            ->selectRaw('SUM(total_sale) AS total_sales')
+            ->groupBy('employee_id');
+
+        $sales_by_employee = Employee::query()
+            ->select('employees.first_name', 'employees.last_name', 'sales.total_sales')
+            ->joinSub($sales, 'sales', function (JoinClause $join) {
+                $join->on('employees.id', '=', 'sales.employee_id');
+            })
+            ->orderBy('employees.last_name', 'ASC')
+            ->get();
+
+        //EJEMPLO 4
+        $users = User::query()
+            ->addSelect([
+                'last_login' => Login::query()->select('created_at')
+                    ->whereColumn('users.id', 'logins.user_id')
+                    ->latest()
+                    ->take(1)
+            ])
+            ->withCasts(['last_login' => 'datetime'])
+            ->get();
+
+        return view('subqueries.index', compact('sales_by_employee', 'users'));
     }
 }
--- resources/views/subqueries/index.blade.php
+++ resources/views/subqueries/index.blade.php
@@ -8,6 +8,22 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
+
+                    {{--@foreach ($projects as $project)
+                    <li>Project: {{ $project->name }} Usuario: {{$project->user->name }}</li>
+                    @endforeach --}}
+
+                    {{--@foreach ($products as $product)
+                    <li>Producto: {{ $product->name }} Precio: {{$product->price }}</li>
+                    @endforeach --}}
+
+                    {{--@foreach ($sales_by_employee as $employee)
+                    <li>Vendedor: {{ $employee->first_name }} {{ $employee->last_name }} Ventas: {{$employee->total_sales }}</li>
+                    @endforeach --}}
+
+                    @foreach ($users as $user)
+                    <li>Usuario: {{ $user->name }} Ultimo Login: {{$user->last_login->locale('es_ES')->diffForHumans() }}</li>
+                    @endforeach
                 </div>
             </div>
         </div>
