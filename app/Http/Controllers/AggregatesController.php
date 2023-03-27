4688f43dc05832d144082ba2a9c35200653189c2
Luis Zelaya <codewithluis11@gmail.com>
2023-03-26 23:02:09 -0600

    Seccion Rendimiento y Consultas Avanzadas: Funciones de Agregado con Relaciones

--- app/Http/Controllers/AggregatesController.php
+++ app/Http/Controllers/AggregatesController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Models\Country;
+use App\Models\Employee;
+use App\Models\Hospital;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\View\View;
 
 class AggregatesController extends Controller
 {
     public function index(): View
     {
-        return view('aggregates.index');
+
+        $employees = Employee::query()
+            ->withCount('sales as number_sales')
+            ->withSum('sales as total_sale_by_employee', 'total_sale')
+            ->withAvg('sales as average_sales', 'total_sale')
+            ->get();
+
+        $countries = Country::query()
+            ->withCount([
+                'hospitals as active_hospitals' => function (Builder $query) {
+                    $query->where('is_active', 1);
+                },
+                'hospitals as inactive_hospitals' => function (Builder $query) {
+                    $query->where('is_active', 0);
+                }
+            ])
+            ->get();
+
+        return view('aggregates.index', compact('employees', 'countries'));
     }
 }
--- resources/views/aggregates/index.blade.php
+++ resources/views/aggregates/index.blade.php
@@ -8,6 +8,48 @@
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
                 <div class="card-body">
+
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Numero Ventas</th>
+                                <th>Total Ventas</th>
+                                <th>Promedio Ventas</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($employees as $employee)
+                            <tr>
+                                <td>{{ $employee->first_name }}</td>
+                                <td>{{ $employee->last_name }}</td>
+                                <td>{{ $employee->number_sales }}</td>
+                                <td>$ {{ number_format($employee->total_sale_by_employee, 2) }}</td>
+                                <td>$ {{ number_format($employee->average_sales, 2) }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+
+                    <table class="table table-bordered table-striped">
+                        <thead>
+                            <tr>
+                                <th>País</th>
+                                <th>Total Hospitales Activos</th>
+                                <th>Total Hospitales Inactivos</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($countries as $country)
+                            <tr>
+                                <td>{{ $country->name }}</td>
+                                <td>{{ $country->active_hospitals }}</td>
+                                <td>{{ $country->inactive_hospitals }}</td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
